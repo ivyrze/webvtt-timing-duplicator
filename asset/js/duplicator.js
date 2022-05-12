74c952d678a0aee095379416c2915898e1d261b9
Ivy Rose <hi@ivyrose.me>
2022-05-11 20:20:16 -0400

    Improved UI and switched to file inputs

--- asset/js/duplicator.js
+++ asset/js/duplicator.js
@@ -1,11 +1,41 @@
 $(document).ready(function () {
-    $("#run").click(run);
+    $("#run").click(function () {
+        if ($("form")[0].reportValidity()) {
+            const readTimings = read($("#timings")[0].files[0]);
+            const readUntimed = read($("#untimed")[0].files[0]);
+            Promise.all([ readTimings, readUntimed ]).then(function (files) {
+                $("#output").val(run(files[0], files[1]));
+            }).catch(function () {
+                console.error("File read error");
+            });
+        }
+    });
+    
+    $("form input").on('change', function () {
+        ($("form")[0].checkValidity()) ?
+            $("#run").removeAttr("disabled") : $("#run").attr("disabled", "");
+    })
 });
 
-function run() {
+function read(file) {
+    const reader = new FileReader();
+    const promise = new Promise(function (resolve, reject) {
+        reader.addEventListener('load', function () {
+            resolve(reader.result);
+        });
+        reader.addEventListener('error', function (event) {
+            reject(event);
+        });
+        reader.readAsText(file);
+    });
+    
+    return promise;
+}
+
+function run(timings, untimed) {
     // Parse input files
     const parser = new WebVTTParser();
-    const parsed = parser.parse($("#timings").val());
+    const parsed = parser.parse(timings);
     
     if (parsed.errors.length > 0) {
         console.error("Parser error", parsed.errors);
@@ -16,7 +46,6 @@ function run() {
     const groupedCues = group(parsed.cues);
     delete parsed;
     
-    const untimed = $("#untimed").val();
     const lines = untimed.split("\n").filter(function (line) {
         return line.trim() != '';
     });
@@ -51,8 +80,7 @@ function run() {
     
     // Serialize output file
     const serializer = new WebVTTSerializer();
-    const output = serializer.serialize(newCues);
-    $("#output").val(output);
+    return serializer.serialize(newCues);
 }
 
 function separate(line) {
